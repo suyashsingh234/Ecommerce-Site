641cf3da3595cd5ff131ed55f1c5dcc9a6d1d8e8
Suyash Singh <suyashsingh234@gmail.com>
2019-08-28 21:01:49 +0530

    minor improvements

---
+++ grocery.js
@@ -0,0 +1,25 @@
+document.getElementById("ls").addEventListener("click",function(){
+	window.location="registration.html";
+});
+
+var minbtn=document.getElementsByClassName('minus');
+
+for(let i=0;i<minbtn.length;i++){
+	minbtn[i].addEventListener('click',function(){
+		var count=this.parentElement.querySelector('.count');
+		var cnt=parseInt(count.innerHTML);
+		if(cnt>0)
+			count.innerHTML=cnt-1;
+		else
+			alert('Cannot be negative!');
+	});
+}
+
+var plusbtn=document.getElementsByClassName('plus');
+
+for(let i=0;i<plusbtn.length;i++){
+	plusbtn[i].addEventListener('click',function(){
+		var count=this.parentElement.querySelector('.count');
+		count.innerHTML=parseInt(count.innerHTML)+1;
+	});
+}
